efcb55b34e8d6257f928f8305fd24879a622ada9
Аделя <112945520+adeelgilfanova@users.noreply.github.com>
2022-09-24 00:01:41 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,54 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Laba3
+namespace Zadachi
 {
-    enum Types
+    struct Student
     {
-        Текущий,
-        Сберегательный
-    }
+        public string Secondname;
+        public string Firstname;
+        public int udentificator;
+        public string data_dr;
+        public string kategoriya_of_alko;
+        public double obiem_of_alko;
 
-    struct Bank
-    {
-        public int number;
-        public string type;
-        public string balance;
-
-        public Bank (int number, string type, string balance)
+        public Student(string Secondname, string Firstname, int udentificator, string data_dr, string kategoriya_of_alko, double obiem_of_alko)
         {
-            this.number = number;
-            this.type = type;
-            this.balance = balance;
+            this.Secondname = Secondname;
+            this.Firstname = Firstname;
+            this.udentificator = udentificator;
+            this.data_dr = data_dr;
+            this.kategoriya_of_alko = kategoriya_of_alko;
+            this.obiem_of_alko = obiem_of_alko;
         }
         public void Print()
         {
-            Console.WriteLine($"Номер: {number}, Тип: {type}, Баланс:{balance}");
-        }       
+            Console.WriteLine($"Фамилия: {Secondname}, Имя: {Firstname}, Идентификатор: {udentificator}, День Рождения: {data_dr}, Категория: {kategoriya_of_alko}, Объем выпитого алкоголя: {obiem_of_alko}");
+        }
     }
-    enum Vus
+    struct Utilizer
     {
-        КГУ,
-        КАИ,
-        КХТИ
-    }
+        public string name_utilizer;
+        public string town;
+        public int age;
+        public int pinkod;
 
-    struct Worker
-    {
-        public string name;
-        public int vuz;
-
-        public Worker (string name, int vuz)
+        public Utilizer(string name_utilizer, string town, int age, int pinkod)
         {
-            this.name = name;
-            this.vuz = vuz;
+            this.name_utilizer = name_utilizer;
+            this.town = town;
+            this.age = age;
+            this.pinkod = pinkod;
         }
         public void Print1()
         {
-            Console.WriteLine($"Имя: {name}, Вуз: {vuz}");
+            Console.WriteLine($"Имя пользователя: {name_utilizer}, Город пользователя: {town}, Возраст: {age}, Пинкод: {pinkod}");
         }
     }
 
@@ -56,34 +54,117 @@ namespace Laba3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Упражнение1");
-            Types credit = Types.Текущий;
-            Types credit1 = Types.Сберегательный;
-            Console.WriteLine("Тип - {0}, номер - {1}", credit, (int)credit);
-            Console.WriteLine("Тип - {0}, номер - {1}", credit1, (int)credit1);
+            
+            Console.WriteLine("Заданий.1");
+            Student student1 = new Student("Илья", "Романов", 1, "01.11.2003", "a", 3.1);
+            student1.Print();
+            Student student2 = new Student("Мария", "Головина", 2, "18.02.2004", "d", 0.01);
+            student2.Print();
+            Student student3 = new Student("Амир", "Сагдуллин", 3, "26.06.2004", "b", 2.3);
+            student3.Print();
+            Student student4 = new Student("Маша", "Мошкина", 4, "16.09.2004", "b", 2.2);
+            student4.Print();
+            Student student5 = new Student("Алина", "Исмагилова", 5, "12.08.2004", "c", 1.3);
+            student5.Print();
+            double obiem_alko = student1.obiem_of_alko + student2.obiem_of_alko + student3.obiem_of_alko + student4.obiem_of_alko + student5.obiem_of_alko;
+            Console.WriteLine($"Первый студент выпил: {Math.Round((student1.obiem_of_alko/obiem_alko)*100,1)}%");
+            Console.WriteLine($"Второй студент выпил: {Math.Round((student2.obiem_of_alko / obiem_alko) * 100, 1)}%");
+            Console.WriteLine($"Третий студент выпил: {Math.Round((student3.obiem_of_alko / obiem_alko) * 100, 1)}%");
+            Console.WriteLine($"Четвертый студент выпил: {Math.Round((student4.obiem_of_alko / obiem_alko) * 100, 1)}%");
+            Console.WriteLine($"Пятый студент выпил: {Math.Round((student5.obiem_of_alko / obiem_alko) * 100, 1)}%");
+            Console.WriteLine($"Общий объем выпитого алкоголя:{obiem_alko} \n");
 
 
-            Console.WriteLine("Упражнение2");
-            Bank bank = new Bank();
-            bank.number = 101234;
-            bank.type = "Текущий";
-            bank.balance = "234560000$";
-            bank.Print();
+            Console.WriteLine("Задание.2");
+            Console.WriteLine($"int - {int.MaxValue} - {int.MinValue}");
+            Console.WriteLine($"double - {double.MaxValue} - {double.MinValue}");
+            Console.WriteLine($"bool - true - false");
+            Console.WriteLine($"byte - {byte.MaxValue} - {byte.MinValue}");
+            Console.WriteLine($"sbyte - {sbyte.MaxValue} - {sbyte.MinValue}");
+            Console.WriteLine($"short - {short.MaxValue} - {short.MinValue}");
+            Console.WriteLine($"ushort - {ushort.MaxValue} - {ushort.MinValue}");
+            Console.WriteLine($"uint - {uint.MaxValue} - {uint.MinValue}");
+            Console.WriteLine($"long - {long.MaxValue} - {long.MinValue}");
+            Console.WriteLine($"ulong - {ulong.MaxValue} - {ulong.MinValue}");
+            Console.WriteLine($"float - {float.MaxValue} - {float.MinValue}");
+            Console.WriteLine($"decimal - {decimal.MaxValue} - {decimal.MinValue}");
+            Console.WriteLine($"string - Ограничено системной памятью");
+            Console.WriteLine($"object - Можно закинуть почти все");
+            Console.WriteLine($"char - {char.MaxValue} - {char.MinValue} \n");
 
-            Console.WriteLine("Домашняя работа1");
-            Vus Vuz1 = Vus.КГУ;
-            Vus Vuz2 = Vus.КАИ;
-            Vus Vuz3 = Vus.КХТИ;
-            Worker worker0 = new Worker("Василий", (int)Vuz1);
-            worker0.Print1();
-            Worker worker1 = new Worker("Николай", (int)Vuz2);
-            worker1.Print1();
-            Worker worker2 = new Worker("Петрович", (int)Vuz3);
-            worker2.Print1();
-            Console.WriteLine("КАИ-0, КХТИ-1 ,КГУ-2");
+
+            Console.WriteLine("Задача.3");
+            Utilizer utilizer = new Utilizer();
+            Console.WriteLine("Введите имя: ");
+            utilizer.name_utilizer = Console.ReadLine();
+            Console.WriteLine("Введите город: ");
+            utilizer.town = Console.ReadLine();
+            Console.WriteLine("Введите возраст: ");
+            utilizer.age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите Пинкод: ");
+            utilizer.pinkod = int.Parse(Console.ReadLine());
+            utilizer.Print1();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Задача.4");
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию:");
+            string surname = Console.ReadLine();
+            string initials = $"{name[0]}.{surname[0]}";
+            Console.WriteLine($"Ваши инициалы:  {initials} \n"); 
+
+            Console.WriteLine("Задача.5");
+            Console.WriteLine("Введите стандартную стоимость бутылки Виски(normPrice):");
+            int normPrice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите скидку на стоимость бутылки Виски(salePrice):");
+            int salePrice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите стоимость отпуска(holidayPrice):");
+            int holidayPrice = int.Parse(Console.ReadLine());
+            double amount_of_bottle = holidayPrice / ((normPrice * salePrice) / 100);
+            if (Math.Round(amount_of_bottle)>amount_of_bottle)
+            {
+                Console.WriteLine(amount_of_bottle-1);
+            }
+            else
+            {
+                Console.WriteLine(amount_of_bottle);
+            }
+
+            Console.WriteLine("\n Задача.6");
+            Console.WriteLine("Введите скорость таракашки в км/ч: ");
+            double speed_of_km = double.Parse(Console.ReadLine());
+            double speed_of_metr = (speed_of_km*27.7778);
+            if (Math.Round(speed_of_metr) > speed_of_metr)
+            {
+                Console.WriteLine(Math.Round(speed_of_metr) - 1);
+            }
+            else
+            {
+                Console.WriteLine(Math.Round(speed_of_metr));
+            }
+
+            Console.WriteLine(" \n Задача.7");
+            Console.WriteLine("Введите строчку: ");
+            string string1 = Console.ReadLine();
+            char[] massOfline = new char[200];
+            int n = 0;
+            int indexOfletter = 0;
+            while (n != string1.Length)
+            {
+                indexOfletter = (int)string1[n];
+                if (indexOfletter >= 65 && indexOfletter <= 90)
+                {
+                    massOfline[n] = (char)(indexOfletter + 32);
+                }
+                if (indexOfletter >= 97 && indexOfletter <= 122)
+                {
+                    massOfline[n] = (char)(indexOfletter - 32);
+                }
+                n++;
+            }
+            Console.WriteLine(massOfline);
             Console.ReadKey();
         }
     }
 }
-
-
